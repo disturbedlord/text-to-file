a27a73a35c231310ec31138811d74abc073e1cd9
elab-developer <todisturbedlord@gmail.com>
2020-05-22 02:05:25 +0530

    updated the UI

---
+++ app.js
@@ -0,0 +1,30 @@
+let text = document.getElementById("textarea");
+let filename = document.getElementById('fileName');
+
+// Downloading the file
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+// Download Button working
+document.getElementById("download_btn").addEventListener("click", function(){
+
+
+
+    download(filename.value(), text.value());
+}, false);
+
+// reset Button working
+document.getElementById("reset_btn").addEventListener("click" , () => {
+  text.value = "";
+  fileName.value = "";
+} , false);
